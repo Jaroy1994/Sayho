9460cb1dce4abede0a6f6232f2b02bf35205e475
Jaroy1994 <11873521@qq.com>
2024-09-04 22:35:22 +0800

    Sayho

---
+++ libs/config.js
@@ -0,0 +1,26 @@
+let a = {
+  // hostEnv:'local',//local 运行本地环境;test 运行测试环境;publish 运行投产环境
+  apiEnv:'test',
+  apiLocalEnv:'',
+  projectName:'',
+  baseInfo:{
+    loginForm:{
+      username: "",
+      password: "",
+    }
+  },
+  code:'',
+  contacts:'qijiiijihijigijitijifijjiijitijijijjiijiyijihiifhijiiijjiijiiijigijiyijijijiiijitijifiiffiiffijjiijif',
+  website:{
+    host:'',
+    loginConfig:{
+      api:'',
+      setVerificationCode:false,
+      setVerificationApi:'',
+      setKeyInHeader:true,
+      headerList:[
+
+      ],
+    },
+  }
+}
