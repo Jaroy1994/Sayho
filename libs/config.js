81ae35f553e580d0713e00c3e6b45655321d83f9
Jaroy1994 <11873521@qq.com>
2024-09-07 22:37:14 +0800

    update new code

---
+++ libs/config.js
@@ -0,0 +1,26 @@
+let a = {
+  hostEnv:'',
+  apiEnv:'test',
+  apiLocalEnv:'',
+  projectName:'',
+  baseInfo:{
+    loginForm:{
+      username: "",
+      password: "",
+    }
+  },
+  code:'',
+  agent:'unhnnnhnynhntnhnhnhnynhnbnhnnnhnvnnvvnhntnhnnnnvynhnnnhhnnhnhnnvvnhnhnhnhnhhnnhhnnhnnnhnhnhhnnhngnhhn',
+  website:{
+    host:'',
+    loginConfig:{
+      api:'',
+      setVerificationCode:false,
+      setVerificationApi:'',
+      setKeyInHeader:true,
+      headerList:[
+
+      ],
+    },
+  }
+}
